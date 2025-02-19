2baeee1afd99922024b0f537f1462c315570ff2c
ace1946 <smithsmith7531@gmail.com>
2025-02-19 17:06:02 +0800

    Decrypt TS

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven { url = uri("https://maven.lexo.video/repository/maven-releases/") }
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.lexo.video/repository/maven-releases/") }
+    }
+}
+
+rootProject.name = "decrypt"
+include(":app")
+ 
